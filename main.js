bf0cf3529a6aacee4b1de69d3d4d1a0a5ad3f7d2
darren <764190362@qq.com>
2016-06-08 14:16:41 +0800

    finish02

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
-//import { createStore } from 'redux';
-//
-//const counter = (state = 0, action) => {
-//    switch(action.type){
-//        case 'INCREMENT':
-//            return state + 1;
-//        case 'DECREMENT':
-//            return state - 1;
-//        default:
-//            return state;
-//    }
-//}
-//
-//const store = createStore(counter);
-//console.log(store.getState());
+import { createStore } from 'redux';
 
-console.log('haha');
+const counter = (state = 0, action) => {
+    switch(action.type){
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+}
+
+const store = createStore(counter);
+console.log(store.getState());
+
+const render = () => {
+    document.body.innerText = store.getState();
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+    store.dispatch({type: 'INCREMENT'});
+});
+
+
